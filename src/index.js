73b651ee3e0ce7f93e3477149b8b44709d3923a5
JuniorBaymax <cooladitya953@gmail.com>
2021-01-14 23:35:23 +0530

    Setting up the React Context API with next Commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { DataLayer } from './DataLayer';
+import reducer, { initialState } from './reducer';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
   </React.StrictMode>,
   document.getElementById('root')
 );
