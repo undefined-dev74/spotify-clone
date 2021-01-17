4abbaba07fa9aba396892e11da619a22aa982c13
JuniorBaymax <cooladitya953@gmail.com>
2021-01-17 23:59:55 +0530

    Design Sidebar icon any layout

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,15 @@
+import { Home, LibraryMusic, Search } from '@material-ui/icons';
 import React from 'react';
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+
 const Sidebar = () => {
+  const data = [
+    { options: 'Home', icon: Home },
+    { options: 'Search', icon: Search },
+    { options: 'Your Library', icon: LibraryMusic },
+  ];
+
   return (
     <div className="sidebar">
       <img
@@ -8,6 +17,11 @@ const Sidebar = () => {
         src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
         alt="Spotify-logo"
       />
+      {data.map(({ options, icon }) => {
+        return <SidebarOption title={options} Icon={icon} />;
+      })}
+      <strong className="sidebar__title">PLAYLIST</strong>
+      <hr />
     </div>
   );
 };
