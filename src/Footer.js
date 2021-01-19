8665f26766c069a0a4fa5441fabe0828b7764605
JuniorBaymax <cooladitya953@gmail.com>
2021-01-19 23:52:10 +0530

    finish styling Footer Component

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,51 @@
+import { Grid, Slider } from '@material-ui/core';
+import {
+  PlayCircleOutline,
+  PlaylistPlay,
+  Repeat,
+  Shuffle,
+  SkipNext,
+  SkipPrevious,
+  VolumeDown,
+} from '@material-ui/icons';
 import React from 'react';
 import './Footer.css';
 const Footer = () => {
-  return <div className="footer">I am foooter</div>;
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img className="footer__albumLogo" src="" alt="" />
+        <div className="footer__songInfo">
+          <h4>Yeah</h4>
+          <p>user</p>
+        </div>
+      </div>
+      <div className="footer__center">
+        <Shuffle className="footer__green" />
+        <SkipPrevious className="footer__icon" />
+        <PlayCircleOutline
+          className="footer__icon"
+          fontSize="large"
+          className="footer_icon"
+        />
+        <SkipNext className="footer__icon" />
+        <Repeat className="footer__green" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlay />
+          </Grid>
+          <Grid item>
+            <VolumeDown />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
